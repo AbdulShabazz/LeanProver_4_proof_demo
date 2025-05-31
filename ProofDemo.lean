f2f541d832485a65b92107543c17e0e7bfacf92c
AbdulShabazz <AbdulShabazz@user.github.com>
2025-05-31 19:26:27 -0400

    modified:   ProofDemo.lean

--- ProofDemo.lean
+++ ProofDemo.lean
@@ -8,9 +8,15 @@ example : ∀ m n : ℕ , Even n → Even (m * n) := by
 
 variable (a b c d e f: ℕ)
 
+section
+
 example (a b c d e f: ℕ) (h : a * b = c * d) (h' : e = f) : a * ( b * e) = c * ( d * f) := by
   -- rw [h']
   -- rw [← mul_assoc]
   -- rw [h]
   -- rw [mul_assoc]
   rw [h', ← mul_assoc, h, mul_assoc]
+
+  end -- end section
+
+  
