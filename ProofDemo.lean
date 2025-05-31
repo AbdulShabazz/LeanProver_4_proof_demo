2be9db97ffdf0868fd35245dfb6545f31c0fc171
AbdulShabazz <AbdulShabazz@user.github.com>
2025-05-31 18:47:42 -0400

    modified:   ProofDemo.lean

--- ProofDemo.lean
+++ ProofDemo.lean
@@ -5,3 +5,12 @@ import Mathlib
 
 example : ∀ m n : ℕ , Even n → Even (m * n) := by
   intros; simp [*, Parser.Attr.parity_simps];
+
+variable (a b c d e f: ℕ)
+
+example (a b c d e f: ℕ) (h : a * b = c * d) (h' : e = f) : a * ( b * e) = c * ( d * f) := by
+  -- rw [h']
+  -- rw [← mul_assoc]
+  -- rw [h]
+  -- rw [mul_assoc]
+  rw [h', ← mul_assoc, h, mul_assoc]
